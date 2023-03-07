cbe4b70245c66996c9cb8d97e3e8c782005af24d
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-03-07 18:04:48 -0300

    User entity and resource
    Aula 342.

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,112 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+//criando a CLASSE/entidade USER... com a INTERFACE SERIALIZABLE... Q serve para transformar
+//OBJETOS em CADEIA de BYTES... Isso serve para trafegar DADOS na rede...
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	//implementando os ATRIBUTOS/VARIAVEIS basicas... do USUARIO
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	
+	//metodo construtor vazio
+	public User() {	
+	}
+
+	
+	//metodo construtor... 
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	
+	
+	//criando os metodos GET e SET... para modificar as VARIAVEIS/ATRIBUTOS
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
